fed84b9500a423f40176318e5a1f98b618e45132
Priyanka <priyanka3007@gmail.com>
2016-08-02 14:04:08 -0400

    ain.js
    weed out a person out of dictionary

---
+++ main.js
@@ -0,0 +1,17 @@
+<script src="main.js">
+var new_ninja = {
+ name: 'Jessica',
+ profession: 'coder',
+ favorite_language: 'JavaScript', //like that's even a question!
+ dojo: 'Dallas'
+}
+var new_ninja = {
+ name: 'Jessica',
+ profession: 'coder',
+ favorite_language: 'JavaScript', //like that's even a question!
+ dojo: 'Dallas'
+}
+for (var prop in new_ninja) {
+  console.log(prop + " = " + new_ninja[prop]);
+}
+</script>
